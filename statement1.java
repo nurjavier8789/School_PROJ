b9a5eabea32ce81bbe46db7f51cc958b44326102
Nur Javier Prasetyo <nurjavierprasetyo8789@gmail.com>
2022-08-24 06:48:00 +0700

    Statement String and Double

---
+++ statement1.java
@@ -0,0 +1,24 @@
+package Profileschool; //your package name
+
+public class test1 { //your class name
+    public static void main (String[] args) {
+        //statement identity
+        String nama,kelas;
+        nama = "Nur Javier Prasetyo";
+        kelas = "X RPL 2";
+        
+        //statement math
+        Double p,l,luas;
+        p=10.0;
+        l=7.5;
+        luas = p*l;
+        
+
+        //print out identity
+        System.out.println("nama: "+ nama);
+        System.out.println("Kelas: "+kelas);
+        
+        //print out math
+        System.out.println("Luas persegi: "+luas+" m bisa digunakan untuk sepakbola");
+    };
+};
