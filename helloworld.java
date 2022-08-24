0ab7b7318c1d98fdae184f7022015801c72145d7
Nur Javier Prasetyo <nurjavierprasetyo8789@gmail.com>
2022-08-24 07:50:12 +0700

    Simple system print out

--- helloworld.java
+++ helloworld.java
@@ -2,6 +2,7 @@ package helloworld; //your package name
 
 public class HelloWorld { //your class name
     public static void main(String[] args) { //must add this line
+        
         System.out.println("baru\nHello World"); //output text
     }
 }
