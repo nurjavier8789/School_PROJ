44244ef7a94cd71778ed345cc1ae3246538b9f7d
Nur Javier Prasetyo <nurjavierprasetyo8789@gmail.com>
2022-08-24 07:29:35 +0700

    Example math
    Example math of:
    - Increase
    - Multiplication
    - Division math
    - Formula for area of a circle
    - Formula for the volume of a triangular pyramid
    - Formula for the volume of a sphere

---
+++ examplemath.java
@@ -0,0 +1,42 @@
+package profileschool;
+
+public class newCode {
+    public static void main (String[] args) {
+        //increase
+        int x,y,hasil;
+        x = 4;
+        y = 8;
+        hasil = x + y;
+        
+        //multiplication
+        int o,e,hasill;
+        o = 10;
+        e = 23;
+        hasill = o * e;
+        
+       double a = 10;
+       double b = 15;
+       double c = a*b;
+       double d = a/b; //division math
+       
+       //formula for area of a circle
+       final double phi = 3.14;
+       double lingkaran = phi * e * e;
+       
+       //formula for the volume of a triangular pyramid
+       double lalas = b*e/2;
+       double limas = y*lalas/3;
+       
+      //formula for the volume of a sphere
+       double bola = 4*phi*e*e*e/3;
+       
+       //display text one by one
+       System.out.println("Hasil dari 4 + 8 = "+hasil);
+       System.out.println("Hasil dari 10 x 23 = "+hasill);
+       System.out.println("Hasil 10 x 15 = "+c);
+       System.out.println("Hasil 10 : 15 = "+d);
+       System.out.println("Hasil Pakai Rumus Lingkaran = "+lingkaran);
+       System.out.println("Hasil Vol. Limas Segitiga = "+limas+"\n");
+       System.out.println("Hasil Vol. Bola= "+bola);
+    };
+}
