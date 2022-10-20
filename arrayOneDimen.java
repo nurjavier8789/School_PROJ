639092faac0d0e05ccd785ee35c9f32b7ce0cac3
Nur Javier Prasetyo <nurjavierprasetyo8789@gmail.com>
2022-10-20 14:36:27 +0700

    How to use Array (One Dimension) Statement

---
+++ arrayOneDimen.java
@@ -0,0 +1,26 @@
+package YOUR_PACKAGE_NAME;
+
+public class YOUR_CLASS_NAME {
+    public static void main(String[] args) {
+        int[] arey = {1,4,9}; // this  how to make array with integer, the order of the array is from 0
+        // example =/{0,1,2};/ this is the order of the array
+        
+        //then how to call the array?
+        //simple... just do it below
+        System.out.println(arey[0]);
+        
+        //so just type the variable of the array, and then pick the order of the array
+        // ...(arey[0]);
+        //          ^ this is the order of the array
+        
+        //you can copy paste it to understand it
+        
+        //same with String
+        //example
+        
+        String[] arrey = {"one(0)","two(1)","three(2)"};
+        //the different is just add the quotation mark and done!
+        //the calling is same
+        System.out.println(arrey[1]);
+    }
+}
